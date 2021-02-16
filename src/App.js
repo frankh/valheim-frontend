87ea1f5b00c958f6606813f3445c68fdc9f623c9
Luke McGregor <lukemcgregor01@gmail.com>
2021-02-16 19:48:52 +0000

    feat: initial frontend write

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./valheim_transparent.png";
+import "./App.css";
+
+const baseUrl = "http://localhost:3001/api";
+
+function apiCall(url, callback) {
+  return fetch(url, {
+    method: "POST",
+  })
+    .then((resp) => {
+      if (resp.ok) {
+        resp.text();
+      }
+    })
+    .then((resp) => {
+      callback(resp);
+    })
+    .catch(() => {
+      callback("Failed");
+    });
+}
 
 function App() {
+  const [status, updateStatus] = useState("Fetching");
+  const [players, updatePlayerList] = useState("Loading");
+  const [playerCount, updatePlayerCount] = useState("Loading");
+  const updateStats = async () => {
+    return Promise.all([
+      apiCall(`${baseUrl}/status`, updateStatus),
+      apiCall(`${baseUrl}/connected`, updatePlayerCount),
+      apiCall(`${baseUrl}/recent`, updatePlayerList),
+    ]);
+  };
+  useEffect(() => {
+    updateStats();
+  }, []);
+  let buttonText;
+  switch (status) {
+    case "Fetching":
+      buttonText = "...";
+      break;
+    case "Failed":
+      buttonText = "Failed to get status";
+      break;
+    case "RUNNING":
+      buttonText = "Server already running...";
+      break;
+    default:
+      buttonText = "Start server";
+      break;
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img src={logo} alt="Valheim logo" />
+        <button
+          className="button"
+          disabled={
+            status === "RUNNING" || status === "Failed" || status === "Fetching"
+          }
+          onClick={() => {
+            updateStatus("Fetching");
+            fetch(`${baseUrl}/start`, {
+              method: "POST",
+            })
+              .then(() => {
+                updateStats();
+              })
+              .catch(() => {
+                updateStats();
+              });
+          }}
         >
-          Learn React
-        </a>
+          {buttonText}
+        </button>
+        <p>Server status: {status}</p>
+        <p>Players connected: {playerCount}</p>
+        <p>Recently connected players: {players}</p>
       </header>
     </div>
   );
